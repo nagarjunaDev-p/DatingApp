4ae18f7525d08933b1958aa662433f93f4ecb8f9
nagarjuna <nagarjunaparuchuri1@gmail.com>
2024-12-30 23:22:51 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(ops =>{
+builder.Services.AddDbContext<DataContext>(ops =>
+{
     ops.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
